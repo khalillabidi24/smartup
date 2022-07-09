8315f1de96633e975d99078a4d2dc3aa162c70ec
Laabidi khalil <58670512+khalillabidi24@users.noreply.github.com>
2022-07-09 16:23:49 +0200

    Update UserTest.java

--- src/test/java/com/example/projetcaisse/UserTest.java
+++ src/test/java/com/example/projetcaisse/UserTest.java
@@ -12,10 +12,10 @@ import org.apache.logging.log4j.Logger;
 
 
 
-import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 import org.springframework.expression.ParseException;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
@@ -24,10 +24,10 @@ import java.util.List;
 
 
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 
-    public class UserTest {
+     class UserTest {
     //private static final Logger l = LogManager.getLogger(UtilisateurImpService.class);
     private static final Logger L = LogManager.getLogger();
     @Autowired
